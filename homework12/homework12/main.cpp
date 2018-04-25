4f8d9d3aad52de15e7f8427942374f442de0dfd7
diego <dgomez1@tulane,edu>
2018-04-24 19:22:24 -0500

    homework12 addition * "Hello world" * Xcode commits from in application attempts

---
+++ homework12/homework12/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  homework12
+//
+//  Created by Diego Gomez on 4/24/18.
+//  Copyright © 2018 Diego Gomez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
