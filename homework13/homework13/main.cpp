c0914d0805249d3afb8b9b14d8595662531e6b70
diego <dgomez1@tulane.edu>
2018-05-03 11:32:13 -0500

    Adding completed homework13

--- homework13/homework13/main.cpp
+++ homework13/homework13/main.cpp
@@ -2,97 +2,90 @@
 #include <fstream>
 #include <cmath>
 #include <iomanip>
+#include <string>
 #include "Projectile.hpp"
 #include "ProjectileWithDrag.hpp"
-using namespace cosc6000;
 
-int main(){
-    const double init_speed = 10.0;
-    const double init_angle = M_PI_4;
-    const double weight1=20;
-    const double D [5] = {0,0.5,1,5,10};
-    const double dt = 0.01;
-    
-    
-    ProjectileWithDrag trajectory0(weight1,init_speed,init_angle,D[0]);
-    ProjectileWithDrag trajectory1(weight1,init_speed,init_angle,D[1]);
-    ProjectileWithDrag trajectory2(weight1,init_speed,init_angle,D[2]);
-    ProjectileWithDrag trajectory3(weight1,init_speed,init_angle,D[3]);
-    ProjectileWithDrag trajectory4(weight1,init_speed,init_angle,D[4]);
-    
-    std::ofstream file0,file1,file2,file3,file4;
-    file0.open("/Users/dgomez/Desktop/COSC6000/homework11/ProjectileData/Projectile0Drag.dat");
-    file1.open("/Users/dgomez/Desktop/COSC6000/homework11/ProjectileData/Projectile1Drag.dat");
-    file2.open("/Users/dgomez/Desktop/COSC6000/homework11/ProjectileData/Projectile2Drag.dat");
-    file3.open("/Users/dgomez/Desktop/COSC6000/homework11/ProjectileData/Projectile3Drag.dat");
-    file4.open("/Users/dgomez/Desktop/COSC6000/homework11/ProjectileData/Projectile4Drag.dat");
-    while(1){
-        file0 << trajectory0 << std::endl;
-        
-        trajectory0.update(dt);
-        
-        vector2d cord = trajectory0.get_coordinate();
-        if (cord.get_y() <= 0.0){
-            break;
-        }
-    }
-    file0.close();
-    while(1){
-        file1 << trajectory1 << std::endl;
-        
-        trajectory1.update(dt);
-        
-        vector2d cord = trajectory1.get_coordinate();
-        if (cord.get_y() <= 0.0){
-            break;
-        }
-    }
-    file1.close();
-    while(1){
-        file2 << trajectory2 << std::endl;
-        
-        trajectory2.update(dt);
-        
-        vector2d cord = trajectory2.get_coordinate();
-        if (cord.get_y() <= 0.0){
-            break;
-        }
-    }
-    file2.close();
-    while(1){
-        file2 << trajectory2 << std::endl;
-        
-        trajectory2.update(dt);
-        
-        vector2d cord = trajectory2.get_coordinate();
-        if (cord.get_y() <= 0.0){
-            break;
-        }
-    }
-    file2.close();
-    while(1){
-        file3 << trajectory3 << std::endl;
-        
-        trajectory3.update(dt);
-        
-        vector2d cord = trajectory3.get_coordinate();
-        if (cord.get_y() <= 0.0){
-            break;
-        }
-    }
-    file3.close();
-    while(1){
-        file4 << trajectory4 << std::endl;
-        
-        trajectory4.update(dt);
-        
-        vector2d cord = trajectory4.get_coordinate();
-        if (cord.get_y() <= 0.0){
-            break;
-        }
-    }
-    file4.close();
+const double weight = 20;
+const double D = 0.5;
+const double dt = 0.01;
+const double init_angle = M_PI_4;
+const double target_dist = 100.;
+
+bool YesOrNo(std::string message){
+    std::cout << message << std::endl;
+    std::string ans;
+    std::cin >> ans;
+    if (ans[0] == 'Y' || ans[0] == 'y') return false;
+    return true;
 }
 
+bool TargetDestroyed(double x,double target){
+    if(fabs(x - target) < 1) return true;
+    return false;
+}
 
+class ProjectileFormatMistake:public std::exception{
+public:
+    ProjectileFormatMistake(std::string response):response(response){}
+    virtual const char *what() const throw(){
+        return response.c_str();
+    }
+private:
+    std::string response;
+};
 
+void simulation(Projectile trajectory){
+    while(1){
+        trajectory.update(dt);
+        vector2d cord = trajectory.get_coordinate();
+        if (cord.get_y() <= 0.0){
+            if (TargetDestroyed(cord.get_x(), target_dist)){
+                std::cout << "Target Destroyed" <<  std::endl;
+            }
+            else {
+                std::cout << "Miss" << std::endl;
+                std::cout << "Final distance is: " << cord.get_x() << "m" << std::endl;
+                std::cout << "A missed by " << fabs(cord.get_x()-target_dist) << "m"
+                << std::endl;
+            }
+            break;
+        }
+    }
+}
+
+int main(){
+    std::cout << "Try to hit a target 100m away" << std::endl;
+    std::cout << "Your object has a weight of " << weight
+    << "kg, " << "an initial angle of " << init_angle <<" and a drag coefficient of "
+    << D << "kg/s" << std::endl;
+   
+    while(1){
+        std::cout << "Enter an initial speed in m/s: ";
+        vector2d cord;
+        double init_speed = 0;
+        std::cin >> init_speed;
+        try {
+            
+            if (!std::cin){
+                std::cin.clear();
+                throw ProjectileFormatMistake("Wrong data type");
+            }
+            if (init_speed <= 0 ){
+                throw ProjectileFormatMistake("Initial speed less than zero!");
+            }
+            ProjectileWithDrag trajectory(weight,init_speed,init_angle,D);
+            simulation(trajectory);
+        }
+        catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+            std::cout << "Enter a resonable speed";
+            continue;
+        }
+        if (YesOrNo("Again?(y/n)")) {
+            break;
+        }
+    }
+    std::cout << "End of program \n";
+    return 0;
+}
