cd203c940c9af35455c521788a4a508bb4224365
diego <dgomez1@tulane.edu>
2018-05-09 13:38:43 -0500

    updated homework13

--- homework13/homework13/main.cpp
+++ homework13/homework13/main.cpp
@@ -18,11 +18,6 @@ bool YesOrNo(std::string message){
     return true;
 }
 
-bool TargetDestroyed(double x,double target){
-    if(fabs(x - target) < 1) return true;
-    return false;
-}
-
 class ProjectileFormatMistake:public std::exception{
 public:
     ProjectileFormatMistake(std::string response):response(response){}
@@ -33,21 +28,24 @@ private:
     std::string response;
 };
 
-void simulation(Projectile trajectory,const double &target_dist){
+bool simulation(Projectile trajectory,const double &target_dist){
     while(1){
         trajectory.update(dt);
         vector2d cord = trajectory.get_coordinate();
         if (cord.get_y() <= 0.0){
-            if (TargetDestroyed(cord.get_x(), target_dist)){
+            if(fabs(cord.get_x() - target_dist) < 1){
                 std::cout << "Target Destroyed" <<  std::endl;
+                return true;
             }
             else {
                 std::cout << "Miss" << std::endl;
-                std::cout << "Final distance is: " << cord.get_x() << "m" << std::endl;
-                std::cout << "A missed by " << fabs(cord.get_x()-target_dist) << "m"
+                std::cout << "Target distance is: " << target_dist << "m" << std::endl;
+                std::cout << "Projectile distance is: " << cord.get_x() << "m" << std::endl;
+                std::cout << "and missed by " << fabs(cord.get_x()-target_dist) << "m"
                 << std::endl;
+                return false;
             }
-            break;
+        break;
         }
     }
 }
@@ -59,42 +57,48 @@ int main(){
     << D << "kg/s" << std::endl;
    
     while(1){
-        vector2d cord;
         double init_speed = 0;
         double init_angle = 0;
         double target_dist = 0;
         std::cout << "\nEnter the target distance: ";
         std::cin >> target_dist;
-        std::cout << "\nEnter an initial speed in m/s: ";
-        std::cin >> init_speed;
-        std::cout << "\nEnter an initial angle in radians: ";
-        std::cin >> init_angle;
-        try {
-            if (!std::cin){
-                std::cin.clear();
-                throw ProjectileFormatMistake("Wrong data!\n");
+        while(1){
+            std::cout << "\nEnter an initial speed in m/s: ";
+            std::cin >> init_speed;
+            std::cout << "\nEnter an initial angle in radians: ";
+            std::cin >> init_angle;
+            try {
+                if (!std::cin){
+                    std::cin.clear();
+                    throw ProjectileFormatMistake("Wrong data!\n");
+                }
+                if (init_speed < 50 || init_speed > 100 ){
+                    throw ProjectileFormatMistake("Initial speed out of bounds!\n");
+                }
+                if (init_angle <= 0 || init_angle >= M_PI_2){
+                    throw ProjectileFormatMistake("Initial angle out of bounds!\n");
+                }
+                if (target_dist <= 0){
+                    throw ProjectileFormatMistake("Target distance less than 0!\n");
+                }
+                ProjectileWithDrag trajectory(weight,init_speed,init_angle,D);
+                if (simulation(trajectory, target_dist)) goto label1;
+                else{
+                    if (YesOrNo("Try again at this distance? (Y/N)")){
+                        goto end;
+                    }
+                }
             }
-            if (init_speed <= 50 || init_speed >= 100 ){
-                throw ProjectileFormatMistake("Initial speed out of bounds!\n");
+            catch (std::exception &e) {
+                std::cout << e.what() << std::endl;
+                std::cout << "Enter correct inputs\n";
+                continue;
             }
-            if (init_angle <= 0 || init_angle >= M_PI_2){
-                throw ProjectileFormatMistake("Initial angle out of bounds!\n");
-            }
-            if (target_dist <= 0){
-                throw ProjectileFormatMistake("Target distance less than 0!\n");
-            }
-            ProjectileWithDrag trajectory(weight,init_speed,init_angle,D);
-            simulation(trajectory,target_dist);
         }
-        catch (std::exception &e) {
-            std::cout << e.what() << std::endl;
-            std::cout << "Enter correct inputs\n";
-            continue;
-        }
-        if (YesOrNo("Again?(y/n)")) {
+    label1:if (YesOrNo("A new target? (Y/N)")) {
             break;
         }
     }
-    std::cout << "End of program \n";
+end:std::cout << "End of program \n";
     return 0;
 }
